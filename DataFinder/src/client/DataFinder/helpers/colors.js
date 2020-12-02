770604760010a3a40bb656bce5501e60396cdf07
Helen Miller <hmiller@fredhutch.org>
2020-12-02 11:18:06 -0800

    color option 3

--- DataFinder/src/client/DataFinder/helpers/colors.js
+++ DataFinder/src/client/DataFinder/helpers/colors.js
@@ -1,10 +1,10 @@
 // colors for plots are muted versions of colors in selectors and buttons
 // export const studyColor = "#bbe2a2" // pistachio
-export const studyColor = "#beb9f6" // periwinkle
-// export const studyColor = "#f3c450" // "#EFB116" // gold
-// export const participantColor =  "#beb9f6" // "#9f97f1" //periwinkle
-export const participantColor = "#7fb9f4" // blue
-export const sampleColor = "#59cb8f" // "#22ba6a" //green 
+// export const studyColor = "#beb9f6" // periwinkle
+export const studyColor = "#f7d88a" // "#EFB116" // gold
+export const participantColor =  "#beb9f6" // "#9f97f1" //periwinkle
+// export const participantColor = "#7fb9f4" // blue
+export const sampleColor = "#90dcb4" // "#22ba6a" //green 
 // export const sampleColor = "#e8a97f" // amber
 // export const sampleColor = "#7fb9f4" // blue
 
