1de92704ca8f8753b9e4bfd37f1846fce7cc159a
Helen Miller <hmiller@fredhutch.org>
2020-12-02 10:52:23 -0800

    color option 2

--- DataFinder/src/client/DataFinder/helpers/colors.js
+++ DataFinder/src/client/DataFinder/helpers/colors.js
@@ -1,9 +1,12 @@
 // colors for plots are muted versions of colors in selectors and buttons
-export const participantColor =  "#beb9f6" // "#9f97f1" //periwinkle
-export const studyColor = "#bbe2a2" // pistachio
+// export const studyColor = "#bbe2a2" // pistachio
+export const studyColor = "#beb9f6" // periwinkle
 // export const studyColor = "#f3c450" // "#EFB116" // gold
-// export const sampleColor = "#59cb8f" // "#22ba6a" //green 
-export const sampleColor = "#e8a97f" // amber
+// export const participantColor =  "#beb9f6" // "#9f97f1" //periwinkle
+export const participantColor = "#7fb9f4" // blue
+export const sampleColor = "#59cb8f" // "#22ba6a" //green 
+// export const sampleColor = "#e8a97f" // amber
+// export const sampleColor = "#7fb9f4" // blue
 
 // export const samplePalette = [
 //     "#FFFFFF",
@@ -16,23 +19,34 @@ export const sampleColor = "#e8a97f" // amber
 // ] // original
 
 // green
-// export const samplePalette = [
-//     "#ffffff",
-//     "#e8f8f0",
-//     "#c7eed9",
-//     "#90dcb4",
-//     "#59cb8f",
-//     "#38c179",
-//     "#22ba6a"
-// ] 
-
-// amber
 export const samplePalette = [
     "#ffffff",
-    "#fbede5",
-    "#f4d4bf",
-    "#e8a97f",
-    "#dc7f40",
-    "#d5651a",
-    "#d15400"
+    "#e8f8f0",
+    "#c7eed9",
+    "#90dcb4",
+    "#59cb8f",
+    "#38c179",
+    "#22ba6a"
 ] 
+
+// amber
+// export const samplePalette = [
+//     "#ffffff",
+//     "#fbede5",
+//     "#f4d4bf",
+//     "#e8a97f",
+//     "#dc7f40",
+//     "#d5651a",
+//     "#d15400"
+// ] 
+
+// blue
+// export const samplePalette = [
+//     "#ffffff",
+//     "#e5f1fd",
+//     "#bfdcfa",
+//     "#7fb9f4",
+//     "#4097ee",
+//     "#1a82eb",
+//     "#0074E9"
+// ]
